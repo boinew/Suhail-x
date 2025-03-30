3ec984613794211faacf4d2260688ef98b74e978
boinew <peterbamidele26@gmail.com>
2025-03-30 23:57:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,234xxxxxxxx";
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,234xxxxxxxx";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "2349116343226";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "null";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
